edb22057a5ddbb3812d8676b2bfecbf586cbe00c
gebrielmbelaineh <gebriel.belaineh.ug@dartmouth.edu>
2021-07-19 13:40:23 -0400

    About page

---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      About
+      <div className="Name">
+        Gebriel Molla Belaineh
+      </div>
+      <div className="Bio">
+        Blah Blah
+      </div>
+      <div className="Contact">
+        <button type="button">Contact us </button>
+      </div>
+      <div className="image">
+        image
+      </div>
+    </div>
+  );
+};
+
+export default About;
